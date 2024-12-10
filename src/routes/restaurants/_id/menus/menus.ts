2e9429677fba902b7817749743b8122d3777008a
Mares1492 <msumarok@tlu.ee>
2024-12-11 00:21:35 +0200

    Delete updateRestaurantMenuById

--- src/routes/restaurants/_id/menus/menus.ts
+++ src/routes/restaurants/_id/menus/menus.ts
@@ -2,11 +2,9 @@ import {FastifyInstance} from "fastify";
 import {
     createRestaurantMenuById,
     getAllRestaurantMenusById,
-    updateRestaurantMenuById
 } from "../../../../controllers/menus";
 
 export default async (fastify: FastifyInstance) => {
     fastify.get('/', getAllRestaurantMenusById);
     fastify.post('/', createRestaurantMenuById);
-    fastify.patch('/', updateRestaurantMenuById)
 }
