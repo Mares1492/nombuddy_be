9d1382bda855d2f7c591c2e385f5c2054c2478d0
Mares1492 <msumarok@tlu.ee>
2024-12-13 00:05:42 +0200

    Switch createRestaurantMenuById to createNewMenu

--- src/routes/restaurants/_id/menus/menus.ts
+++ src/routes/restaurants/_id/menus/menus.ts
@@ -1,10 +1,10 @@
 import {FastifyInstance} from "fastify";
 import {
-    createRestaurantMenuById,
+    createNewMenu,
     getAllRestaurantMenusById,
 } from "../../../../controllers/menus";
 
 export default async (fastify: FastifyInstance) => {
     fastify.get('/', getAllRestaurantMenusById);
-    fastify.post('/', createRestaurantMenuById);
+    fastify.post('/', createNewMenu);
 }
