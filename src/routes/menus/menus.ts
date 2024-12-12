97f91e05e6f83b49c02e20a77b595aa732fbdfc3
Mares1492 <msumarok@tlu.ee>
2024-12-13 00:07:31 +0200

    Add CRUD methods
    - createNewMenu
    - updateMenuById
    - deleteMenuById
    - delete updateRestaurantMenuById

--- src/routes/menus/menus.ts
+++ src/routes/menus/menus.ts
@@ -1,8 +1,15 @@
-import {getAllMenus, getMenuById, updateRestaurantMenuById} from "../../controllers/menus";
+import {
+    createNewMenu, deleteMenuById,
+    getAllMenus,
+    getMenuById,
+    updateMenuById,
+} from "../../controllers/menus";
 import {FastifyInstance} from "fastify";
 
 export default async (fastify: FastifyInstance) => {
     fastify.get('/', getAllMenus);
+    fastify.post('/', createNewMenu);
     fastify.get('/:menuId', getMenuById);
-    fastify.patch('/:menuId', updateRestaurantMenuById)
+    fastify.patch('/:menuId', updateMenuById)
+    fastify.delete('/:menuId', deleteMenuById);
 }
