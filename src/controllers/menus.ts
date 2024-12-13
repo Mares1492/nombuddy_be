6aad8658e91f5345dcdc5cfeffef85eab29dd655
Mares1492 <msumarok@tlu.ee>
2024-12-13 02:35:14 +0200

    Move query in getAllRestaurantMenusById
    -  move query  to service layer

--- src/controllers/menus.ts
+++ src/controllers/menus.ts
@@ -2,6 +2,7 @@
 import {FastifyReply, FastifyRequest} from "fastify";
 import {RestoMenu, RestoParams} from "../types/global";
 import {prisma} from "../index";
+import {getRestaurantMenus} from "../services/menus";
 
 const returnErrorMessage = (message:string) => ({body:{},message:message});
 
@@ -14,16 +15,13 @@ export const getAllMenus = async (request:FastifyRequest<{ Params: RestoParams }
     reply.send({body:menus,message:"Found menus"});
 };
 
+
 export const getAllRestaurantMenusById = async (request:FastifyRequest<{ Params: RestoParams }>, reply:FastifyReply) => {
-    // Fetch menu from restaurant
     const { id } = request.params;
-    const menus:Array<RestoMenu> = await prisma.$queryRaw`
-        SELECT *
-        FROM menu m
-        JOIN restaurant_menu rm ON m.id = rm.menu_id
-        WHERE rm.restaurant_id = ${Number(id)};
-    `;
-    if (!menus || menus.length === 0) {
+    // TODO: create input validator
+    // getRestaurantMenus from menus service
+    const menus:RestoMenu[] = await getRestaurantMenus(Number(id))
+    if (!menus.length) {
         reply.send(returnErrorMessage("No restaurant found"));
     }
     reply.send({body: menus,message:`Menus found for resto-${id}`});
@@ -50,8 +48,11 @@ export const updateMenuById = async (request:FastifyRequest<{ Params: RestoParam
     if (!menu) {
         reply.send(returnErrorMessage(`No menu found with given id-${menuId}`));
     }
-    // TODO: update menu
-    reply.send({body:menu,message:"Menu updated"});
+    // TODO: get updated data from body
+    // TODO: check if has right to do updates
+    // const updatedMenu = await prisma.menu.update()
+    // TODO: update resto menu data
+    reply.send({body:menu,message:"Menu updated(actually not)"});
 }
 
 export const deleteMenuById = async (request:FastifyRequest<{ Params: RestoParams }>, reply:FastifyReply) => {
@@ -71,13 +72,3 @@ export const createNewMenu = async (request:FastifyRequest<{ Params: RestoParams
     // Example: const newMenu = await prisma.menu.create({ data: { ...request.body, restaurantName: restoName } });
     return {body:{},message:`Added new menu for restaurant ${id}`};
 };
-
-export const updateRestaurantMenuById = async (request:FastifyRequest<{ Params: RestoParams }>, reply:FastifyReply) => {
-    const {menuId} = request.params
-    const body = request.body
-    // TODO: get updated data from body
-    // TODO: check if has right to do updates
-    // const updatedMenu = await prisma.menu.update()
-    // TODO: update resto menu data
-    return reply.send({body:{},message:`Update menu ${menuId} (actually not)`});
-}
