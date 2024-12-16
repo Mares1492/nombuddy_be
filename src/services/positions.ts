3061f95a585391ac2c8228cb0811f5f1b36c219b
Mares1492 <msumarok@tlu.ee>
2024-12-16 02:08:11 +0200

    Add getPositionData

--- src/services/positions.ts
+++ src/services/positions.ts
@@ -4,3 +4,7 @@ export const getPositionsData = async () => {
     // TODO: instead of simple positions fetch complex data with position + options + category + menu
     return prisma.position.findMany();
 }
+
+export const getPositionData = async (id:number) => {
+    return prisma.position.findUnique({where: {id:id}});
+}
