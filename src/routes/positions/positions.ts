d5ce1ce5bff55a6d848ac7beab0d4d760ca4e130
Mares1492 <msumarok@tlu.ee>
2024-12-25 00:24:30 +0200

    add createNewPosition path

--- src/routes/positions/positions.ts
+++ src/routes/positions/positions.ts
@@ -1,8 +1,8 @@
 import {FastifyInstance} from "fastify";
-import {getAllPositions, getPositionById} from "../../controllers/positions";
+import {createNewPosition, getAllPositions, getPositionById} from "../../controllers/positions";
 
 export default async (fastify: FastifyInstance) => {
     fastify.get('/',getAllPositions)
     fastify.get('/:posId',getPositionById)
-    fastify.post('/',()=>{})
+    fastify.post('/menuId',createNewPosition)
 }
