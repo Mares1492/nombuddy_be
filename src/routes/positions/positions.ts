1d339209d5a10d629c2e13b1fbec6fb188644d36
Mares1492 <msumarok@tlu.ee>
2024-12-27 20:46:03 +0200

    Add PATCH /positions/:id path

--- src/routes/positions/positions.ts
+++ src/routes/positions/positions.ts
@@ -1,9 +1,16 @@
 import {FastifyInstance} from "fastify";
-import {createNewPosition, deletePositionById, getAllPositions, getPositionById} from "../../controllers/positions";
+import {
+    createNewPosition,
+    deletePositionById,
+    getAllPositions,
+    getPositionById,
+    updatePositionById
+} from "../../controllers/positions";
 
 export default async (fastify: FastifyInstance) => {
     fastify.get('/',getAllPositions)
     fastify.get('/:id',getPositionById)
     fastify.post('/',createNewPosition)
+    fastify.patch('/:id',updatePositionById)
     fastify.delete('/:id',deletePositionById)
 }
