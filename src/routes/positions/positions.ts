fc9f5cce4646144beba367177bfbea9717998b77
Mares1492 <msumarok@tlu.ee>
2024-12-27 18:46:24 +0200

    Update positions.ts
    - change posId to id
    - add path DELETE /positions/:id

--- src/routes/positions/positions.ts
+++ src/routes/positions/positions.ts
@@ -1,8 +1,9 @@
 import {FastifyInstance} from "fastify";
-import {createNewPosition, getAllPositions, getPositionById} from "../../controllers/positions";
+import {createNewPosition, deletePositionById, getAllPositions, getPositionById} from "../../controllers/positions";
 
 export default async (fastify: FastifyInstance) => {
     fastify.get('/',getAllPositions)
-    fastify.get('/:posId',getPositionById)
+    fastify.get('/:id',getPositionById)
     fastify.post('/',createNewPosition)
+    fastify.delete('/:id',deletePositionById)
 }
