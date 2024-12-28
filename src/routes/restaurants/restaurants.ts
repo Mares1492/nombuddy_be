546e5cabc8cc8d7411fbf33d09749102475cb88e
Mares1492 <msumarok@tlu.ee>
2024-12-28 17:19:45 +0200

    Move getRestaurantById to path /restaurants/:id

--- src/routes/restaurants/restaurants.ts
+++ src/routes/restaurants/restaurants.ts
@@ -1,8 +1,7 @@
-import {createRestaurant, getRestaurantById, getAllRestaurants} from "../../controllers/restaurants";
+import {createRestaurant, getAllRestaurants} from "../../controllers/restaurants";
 import {FastifyInstance} from "fastify";
 
 export default async (fastify: FastifyInstance) => {
     fastify.get('/', getAllRestaurants);
-    fastify.get('/:restoId', getRestaurantById);
     fastify.post('/', createRestaurant);
 }
