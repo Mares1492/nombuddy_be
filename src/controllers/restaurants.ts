27e2e6c077458b663994fd98c137ae2c3349205d
Mares1492 <msumarok@tlu.ee>
2025-01-01 07:02:17 +0200

    Update deleteRestaurantById
    - add dependency deletions

--- src/controllers/restaurants.ts
+++ src/controllers/restaurants.ts
@@ -2,6 +2,7 @@
 import {FastifyReply, FastifyRequest} from "fastify";
 import type {CreateRestaurantBody, RestoParams} from "../types/global";
 import {prisma} from "../index";
+import {returnErrorMessage} from "../utils/errorHandlers";
 
 export const getAllRestaurants = async (request:FastifyRequest<{ Params: RestoParams }>, reply:FastifyReply) => {
     // Fetch all restaurants
@@ -22,7 +23,7 @@ export const createRestaurant = async (request:FastifyRequest<{ Params: RestoPar
             discount_code: crypto.randomUUID()
     }})
     const newRestaurantPerson = await prisma.restaurant_person.create({data:{
-            person_id: request.body.admin_id,
+            person_id: request.body.creators_person_id,
             restaurant_role_id: newRestaurantRole.id,
     }})
     // TODO: create additional references
@@ -52,11 +53,21 @@ export const updateRestaurantById = async (request:FastifyRequest<{ Params: Rest
 
 export const deleteRestaurantById = async (request:FastifyRequest<{ Params: RestoParams }>, reply:FastifyReply) => {
     const {id} = request.params;
-    const restaurant = await prisma.restaurant.delete({ where: { id:Number(id) } });
-    if (!restaurant) {
-        return {body:{},message:"No restaurant found"};
+    const restoID = Number(id)
+    const restaurantToDelete = await prisma.restaurant.findFirst({ where: { id:restoID } });
+    if (!restaurantToDelete) {
+        reply.send(returnErrorMessage("No restaurant found"));
     }
+    const restoRoles = await prisma.restaurant_role.findMany({ where: { restaurant_id:restoID } });
+    // TODO: optimize
+    for (const restoRole of restoRoles) {
+        await prisma.restaurant_person.deleteMany({where:{restaurant_role_id:restoRole.id}});
+    }
+    await prisma.$transaction([
+        prisma.restaurant_role.deleteMany({where:{restaurant_id:restoID}}),
+        prisma.restaurant.delete({ where: { id: restoID} })
+    ])
     // TODO: check if has right to do so
-    // TODO: delete resto
-    return reply.send({body:{},message:"Updated restaurant data(actually not)"});
+
+    reply.send({message:`Deleted restaurant: ${restaurantToDelete!.formal_name} | ${restaurantToDelete!.display_name}`, body:{}});
 }
