014bf2d3bc55de69d03577e6a0053d043c99b8f6
Mares1492 <msumarok@tlu.ee>
2025-01-02 06:06:29 +0200

    Add CRUD routes
    - add getOrderById
    - add updateOrderById
    - add deleteOrderById

--- src/routes/orders/orders.ts
+++ src/routes/orders/orders.ts
@@ -1,6 +1,9 @@
 import {FastifyInstance} from "fastify";
-import {getRestaurantOrderById} from "../../controllers/orders";
+import {deleteOrderById, getOrderById, getRestaurantOrderById, updateOrderById} from "../../controllers/orders";
 
 export default async (fastify: FastifyInstance) => {
     fastify.get('/', getRestaurantOrderById);
+    fastify.get('/', getOrderById);
+    fastify.patch('/:id', updateOrderById);
+    fastify.delete('/:id',deleteOrderById)
 }
