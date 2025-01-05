a8d88b13db99c046b341cfeee547d1b43e9aa741
Mares1492 <msumarok@tlu.ee>
2025-01-05 18:05:20 +0200

    Delete getRestaurantOrderById from path
    - dublicate path

--- src/routes/orders/orders.ts
+++ src/routes/orders/orders.ts
@@ -1,8 +1,7 @@
 import {FastifyInstance} from "fastify";
-import {deleteOrderById, getOrderById, getRestaurantOrderById, updateOrderById} from "../../controllers/orders";
+import {deleteOrderById, getOrderById, updateOrderById} from "../../controllers/orders";
 
 export default async (fastify: FastifyInstance) => {
-    fastify.get('/', getRestaurantOrderById);
     fastify.get('/', getOrderById);
     fastify.patch('/:id', updateOrderById);
     fastify.delete('/:id',deleteOrderById)
